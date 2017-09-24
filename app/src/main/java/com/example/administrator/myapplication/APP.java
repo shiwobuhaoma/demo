69b5de5ece195026fa51a98c4eabaec13940a57e
lxf <1009281661@qq.com>
2017-09-24 13:37:26 +0800

    1、增加application类

---
+++ app/src/main/java/com/example/administrator/myapplication/APP.java
@@ -0,0 +1,11 @@
+package com.example.administrator.myapplication;
+
+import android.app.Application;
+
+/**
+ * lxf
+ * Created by Administrator on 2017/8/31.
+ */
+
+public class APP extends Application {
+}
